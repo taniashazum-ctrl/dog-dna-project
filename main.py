818711eeb7739421deafad158ce76b2898c79ee9
taniashazum-ctrl <tania.shazum@gmail.com>
2026-03-24 10:34:32 +0000

    Initial commit: load sequences
    - Added load_sequences function
    - Reads FASTA files using Biopython
    - Prepares data for downstream analysis

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 Dog DNA Project
+
+# LOAD SEQUENCES
+
+
+def load_sequences(database_file: str, mystery_file: str) -> Tuple[List[SeqRecord], SeqRecord]:
+    """
+    Load DNA sequences from FASTA files.
+
+    Args:
+        database_file (str): File containing known dog breed sequences
+        mystery_file (str): File containing unknown sequence
+
+    Returns:
+        Tuple[List[SeqRecord], SeqRecord]:
+            - List of known breed sequences
+            - Mystery sequence
+    """
+    database = list(SeqIO.parse(database_file, "fasta"))
+    mystery = next(SeqIO.parse(mystery_file, "fasta"))
+    return database, mystery
+ 
